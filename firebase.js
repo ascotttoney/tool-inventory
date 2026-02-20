bb2e2a108e1905acdf319597c1246b0386169203
Scott Toney <ascotttoney@gmail.com>
2026-02-20 13:34:32 -0500

    added Firebase CLI

--- firebase.js
+++ firebase.js
@@ -1,14 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import * as googleapi from './.env'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: googleapi,
+  apiKey: "AIzaSyA0KDx1DC-C4e5inEYD3GGvsBGXsduPb5c",
   authDomain: "toney-tool-inventory.firebaseapp.com",
   projectId: "toney-tool-inventory",
   storageBucket: "toney-tool-inventory.firebasestorage.app",
@@ -17,6 +11,7 @@ const firebaseConfig = {
   measurementId: "G-07LMZPCN2G"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
